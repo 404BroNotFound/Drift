061e86471aabf00756ee4a8090c2436ee4b5d013
404BrainNotFound <u2m0a1r3ktk@gmail.com>
2026-07-16 15:23:57 +0500

    Start page at top on initial load

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+if ("scrollRestoration" in history) history.scrollRestoration = "manual";
+if (!window.location.hash) {
+  window.scrollTo(0, 0);
+  window.addEventListener("pageshow", () =>
+    requestAnimationFrame(() => window.scrollTo(0, 0)),
+  );
+}
+
 const sounds = [
   {
     title: "Moonlit Shore",
